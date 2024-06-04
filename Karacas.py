495f5b375d6e4d56b31ffe28a63baf890f6c81c4
Kyxlii <144890866+Kyxlii@users.noreply.github.com>
2024-06-04 15:56:19 +0100

    Improve Karacaws

--- Karacas.py
+++ Karacas.py
@@ -1,23 +1,28 @@
-##funcs
-def encrypt(unencrypted): #Technically incorrect following of steps :P
-    encrypted = ""
-    for x in unencrypted: #Here x == unencrypted[x].
-        if x in vowelKey: #If x matches any primary key in the dictionary \/
-            encrypted += vowelKey[x]    #concatinates the encrypted letter into a variable
-        else:
-            encrypted += x #If x doesn't match, concatinates it as is into the variable
-    return encrypted+"aca" #concatinates "aca" to the completed encryption and returns to call.
+#Encryption and decryption section
 
-##main
+def encrypt(unencrypted):
+    '''Seperates the text into words, reverses the word, encrypts the vowels into numbers, appends "aca" to the word, moves onto next word.'''
+    return " ".join(["".join([vowelKey[x] if x in vowelKey else x for x in word[::-1]])+"aca" for word in unencrypted])
 
-vowelKey = { #Dictionary for the encryption key.
+def decrypt(encrypted):
+    '''Seperates the text into words, removes "aca" from the word, decrypts the numbers into vowels, reverses the word, moves onto next word.'''
+    return " ".join(["".join([keyVowel[x] if x in keyVowel else x for x in word[:-3]])[::-1] for word in encrypted])
+
+
+vowelKey = { #Dictionary for the encryption key. Technically incorrect as both i & o are "2" in task description, however this removes errors in decryption.
     "a" : "0",
     "e" : "1",
     "i" : "2",
-    "o" : "2",
-    "u" : "3"
+    "o" : "3",
+    "u" : "4"
 }
+keyVowel = {value : key for key, value in vowelKey.items()} #Dictionary for the decryption key. Uses dictionary comprehension to reverse vowelKey
 
-userWord = input("Which word would you like to encrypt?\n") #User input.
 
-print(f'Your encrypted word is: "{encrypt(userWord[::-1].lower())}"') #[::-1] uses string logic to reverse the word.
+#Input and output section
+
+userText = input("Text to Karacas:\n").lower().split() #User input. Splits the user's text into an array of strings.
+
+funcType = ["decrypted", decrypt] if all([word[-3:] == "aca" for word in userText]) else ["encrypted", encrypt]
+
+print(f'Your {funcType[0]} {"word" if len(userText) == 1 else "sentence"} is: \"{funcType[1](userText)}\"') 
