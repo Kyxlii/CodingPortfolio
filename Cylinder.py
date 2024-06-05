b9c6ed58d2afc71ce6180d1820da556c494bb4d4
Kyxlii <144890866+Kyxlii@users.noreply.github.com>
2024-06-05 13:56:30 +0100

    Delete Cylinder.py

--- Cylinder.py
+++
@@ -1,4 +0,0 @@
-iRadius = float(input("Please enter the radius of your cylinder(cm)\n"))
-iDepth = float(input("Please enter the depth of your cylinder(cm)\n"))
-
-print(f"The volume of your cylinder is {round((3.14159*iRadius**2)*iDepth, 3)}cm^3")
